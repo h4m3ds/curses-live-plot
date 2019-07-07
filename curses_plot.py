0319d6534d8c811f705a39c972bbb5c39c337774
h4m3ds <47044840+h4m3ds@users.noreply.github.com>
2019-07-07 03:58:19 +0200

    Update curses_plot.py

--- curses_plot.py
+++ curses_plot.py
@@ -26,8 +26,8 @@ def main(stdscr):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind(("",4353))
-    arrSize = 100
-    arr = np.full(arrSize, np.nan)
+    arrSize = 500
+    arr = np.array(np.full(arrSize, -1000.0))
 
     # log file
     with open('log_file.csv', mode='w') as log:
@@ -39,7 +39,7 @@ def main(stdscr):
     ind = arrSize-1
     tt1=tt2=hh1=hh2=0
     while 1:
-        ind = ind-1 
+        ind = ind-1
         if ind< 0:
             ind = arrSize-1 
         #arr[ind] = np.sin((ind/10.0))*mul+50
@@ -52,13 +52,10 @@ def main(stdscr):
         #else:
         #    tt2 = tt; hh2=hh
         #arr[ind] = np.abs(hh1-hh2)  
-        arr[ind] = np.sin((ind/100.0))*mul + 300
-
+        arr[ind] = np.sin((ind/10.0))*(mul) + 500.0
         plot_array.plot(stdscr,arr,ind)
         time.sleep(.1)    
     #stdscr.getkey()
-
 print(12)
 time.sleep(0.2)
-
 wrapper(main)
